c68276d650c8030c99452cbcaeb1b50ed08a35a9
奥 雄祐 <yusuke_oku@youfit.co.jp>
2026-08-27 11:09:46 +0900

    faviconを追加、next.config.tsの不要な設定を削除

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   agentRules: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.slprecordings.com",
-        pathname: "/wp-content/uploads/**",
-      },
-    ],
-  },
   async redirects() {
     return [
       // 旧: 専用ページとして実装していた際の内部URL → 現行のURLへの保険用リダイレクト
